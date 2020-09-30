b54b00d04f7d0ac10096135f7732f8ddb2f051fe
To Mikha <toustsikmikhail@gmail.com>
2020-09-30 15:52:07 +0300

    feat:add min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let tmp = array[0];
 
-exports.min = function min (array) {
-  return 0;
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] < tmp) {
+                tmp = array[i];
+            }
+        }
+        return tmp;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let tmp = array[0];
+
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] > tmp) {
+                tmp = array[i];
+            }
+        }
+        return tmp;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+
+
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let tmp = 0;
+        let result;
+
+        for (let i = 0; i < array.length; i++) {
+            tmp += array[i];
+        }
+
+        result = tmp / array.length;
+        return result;
+    }
 }
